72d5c81d7cb882738692b8ba7bd22625284f5573
Robbie Pitts <me@robbie-pitts.com>
2020-10-14 16:44:42 -0400

    kpup: update 0.1.4 bottle.

--- Formula/kpup.rb
+++ Formula/kpup.rb
@@ -5,6 +5,12 @@ class Kpup < Formula
   sha256 "ef059cb371fd6937fcd730d6c9f9e3a7ca38fc97066e089027dec806f49aaafe"
   license "MIT"
 
+  bottle do
+    root_url "https://dl.bintray.com/pittst3r/bottles-formulae"
+    cellar :any_skip_relocation
+    sha256 "94a1680c2884b3d5a0c25db669c717fa05a3f01e80bcbbb3037054fe59d21c3c" => :catalina
+  end
+
   depends_on "rust" => :build
 
   def install
